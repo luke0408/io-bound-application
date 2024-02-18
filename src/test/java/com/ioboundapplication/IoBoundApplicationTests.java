66a572ab098d2d0615a1021ad7787b4461127740
SunKyu Choi <zjvl324@gmail.com>
2024-02-18 18:10:25 +0900

    :tada: Feat: Project init

---
+++ src/test/java/com/ioboundapplication/IoBoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioboundapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoBoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
